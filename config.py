897a6e2f94f0232d961d5729016a521e9ff6dacc
Batosay77 <142894951+Batosay77@users.noreply.github.com>
2025-04-28 13:06:08 +0700

    Update config.py
    Signed-off-by: Batosay77 <142894951+Batosay77@users.noreply.github.com>

--- config.py
+++ config.py
@@ -60,6 +60,7 @@ TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
+COOKIES = getenv("COOKIES", "")
 
 # Get your pyrogram v2 session from @StringFatherBot on Telegram
 STRING1 = getenv("STRING_SESSION", None)
